3c78e67647b2a072b88ff6a760d382a7347be29d
SYDAN <dmagma@hotmail.com>
2020-04-11 18:32:21 -0700

    Create 04-NLP-Pandas.py
    Fourth Module of NLP Crash Course: Pandas

---
+++ 04-NLP-Pandas.py
@@ -0,0 +1,97 @@
+#-----------NLP Crash Course--------------
+#------CMPT 340: Biomedical Imaging-------
+#-----------------------------------------
+#-----------------------------------------
+#-----------------------------------------
+#--------------SECTION 4:-----------------
+#----------------Pandas--------------------
+#*****************************************
+#---Overview of sections:-----------------
+#---4.1 Importing Libraries
+#---4.2 Series Function
+#---4.3 Operator functions: any and all
+#---4.4 Apply function
+#---4.5 Converting types
+#---4.6 Copy function
+#---4.7 Dataframes
+
+#*****************************************
+#*****************************************
+
+# Introduction to pandas
+# 4.1 Importing Libraries
+import pandas as pd
+import numpy as np
+
+# 4.2 Series Function
+# pd.Series(self, data=None, index=None, dtype=None, name=None, copy=False, fastpath=False)
+
+x = pd.Series([1,2,3,4,5])
+
+x + 100
+
+(x ** 2) + 100
+
+x > 2
+
+
+# 4.3 Operator functions: any and all
+larger_than_2 = x > 2
+
+larger_than_2.any()
+
+larger_than_2.all()
+
+# 4.4 Apply function
+def f(x):
+    if x % 2 == 0:
+        return x ** 2
+    else:
+        return x ** 3
+
+x.apply(f)
+
+# 4.5 Converting types
+x.astype(np.float64)
+
+# 4.6 Copy function
+x
+y = x
+y[0] = 100
+
+x = pd.Series([1,2,3,4,5])
+y = x.copy()
+
+y[0] = 100
+y
+
+# 4.7 Dataframes
+# pd.DataFrame(self, data=None, index=None, columns=None, dtype=None, copy=False)
+
+data = [1,2,3,4,5,6,7,8,9]
+df = pd.DataFrame(data, columns=["x"])
+df
+
+# One column as a series
+dataSeries = df["x"]
+
+# Adding more columns
+df["x_plus_2"] = df["x"] + 2
+df
+
+df["x_square"] = df["x"] ** 2
+df["x_factorial"] = df["x"].apply(np.math.factorial)
+df
+
+df["is_even"] = df["x"] % 2
+df
+
+# Dropping/Deleting columns
+df = df.drop("is_even", 1)
+df
+
+# Selecting multiple columns
+df[["x","x_plus_2"]]
+
+# Describing
+df.describe()
